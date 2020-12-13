402f3e5bd4dfefed38c4f44efb323133c06429ad
hasezoey <hasezoey@gmail.com>
2020-12-14 00:08:18 +0100

    fix(client-cli): add basic logging & clap construction

--- crates/client-cli/src/main.rs
+++ crates/client-cli/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello from Client");
+use clap::load_yaml;
+use clap::App;
+use flexi_logger::Logger;
+use log::debug;
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+	Logger::with_env_or_str("debug").start()?; // TODO: change this to "error" when ready
+	let yml = load_yaml!("../cli.yml");
+	let _cli_matches = App::from_yaml(yml).get_matches();
+
+	debug!("Hello from Client (Debug)");
+	println!("Hello from Client");
+	
+	return Ok(());
 }
