a818ffaad09bebf3d1297be7e0e4aabeeffd6207
hasezoey <hasezoey@gmail.com>
2020-12-17 18:32:26 +0100

    fix(daemon-cli.main): add context to Result-Errors

--- crates/daemon-cli/src/main.rs
+++ crates/daemon-cli/src/main.rs
@@ -2,14 +2,15 @@ use clap::load_yaml;
 use clap::App;
 // use rss_reader_daemon_core::locks::try_lock;
 use flexi_logger::Logger;
+use anyhow::Context;
 
 mod config;
 
 fn main() -> anyhow::Result<()> {
-	Logger::with_env_or_str("debug").start()?; // TODO: change this to "error" when ready
+	Logger::with_env_or_str("debug").start().context("Logger failed to start")?; // TODO: change this to "error" when ready
 	let yml = load_yaml!("../cli.yml");
 	let cli_matches = App::from_yaml(yml).get_matches();
-	let config = config::BinConfig::from_cli_matches(&cli_matches)?;
+	let config = config::BinConfig::from_cli_matches(&cli_matches).context("Creating config from cli/env failed")?;
 
 	// try_lock(&config.lib_config)?;
 
