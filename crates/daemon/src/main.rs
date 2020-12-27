ed3dcb67de4d7bf2f7d099c18abc27748a5287a3
hasezoey <hasezoey@gmail.com>
2020-12-28 00:39:55 +0100

    feat(main): add tokio & warp
    - add tokio as async runtime
    - add tokio task to run at an interval (sleep)
    - add basic warp http server

--- crates/daemon/src/main.rs
+++ crates/daemon/src/main.rs
@@ -1,19 +1,38 @@
+mod lib;
+use std::net::SocketAddrV4;
+
 use anyhow::Context;
 use clap::load_yaml;
 use clap::App;
 use flexi_logger::Logger;
-use log::info;
-
-mod lib;
 use lib::*;
+use log::{
+	debug,
+	info,
+};
+use tokio::time;
+use warp::Filter;
 
-fn main() -> anyhow::Result<()> {
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
 	Logger::with_env_or_str("debug")
 		.start()
 		.context("Logger failed to start")?; // TODO: change this to "error" when ready
 	let yml = load_yaml!("../cli.yml");
 	let cli_matches = App::from_yaml(yml).get_matches();
 	let config = config::Config::from_cli_matches(&cli_matches).context("Creating config from cli/env failed")?;
+
+	tokio::task::spawn(async move {
+		debug!("inside sleep-interval task");
+		loop {
+			time::sleep(time::Duration::from_secs(10)).await;
+			debug!("sleep-interval");
+		}
+	});
+
+	let routes = warp::any().map(|| "Hello there");
+
+	warp::serve(routes).run(SocketAddrV4::new(config.ip, config.port)).await;
 
 	info!("daemon end");
 	return Ok(());
