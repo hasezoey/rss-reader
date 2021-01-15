553a04ada0576b798f862c6961b8accd30f4a8a3
hasezoey <hasezoey@gmail.com>
2021-01-15 22:30:51 +0100

    style: fix clippy

--- crates/daemon/src/lib/routes.rs
+++ crates/daemon/src/lib/routes.rs
@@ -5,9 +5,9 @@ use warp::{
 };
 
 pub fn get_routes(_config: &crate::config::Config) -> BoxedFilter<(impl Reply,)> {
-	let root_path = warp::path::end().map(|| "Hello Root");
-	let test1 = warp::path::end().map(|| "Hello Test1");
-	let test2 = warp::path("nested").and(warp::path::end()).map(|| "Hello test2");
+	let root_path = warp::path::end().map(|| return "Hello Root");
+	let test1 = warp::path::end().map(|| return "Hello Test1");
+	let test2 = warp::path("nested").and(warp::path::end()).map(|| return "Hello test2");
 
 	let testy = warp::path("test").and(test1.or(test2));
 	return warp::get().and(root_path.or(testy)).with(warp::log("*")).boxed();
