129e7b4225300779ec6e5dde632d141fd8ec1389
hasezoey <hasezoey@gmail.com>
2021-01-15 22:12:20 +0100

    chore(clippy.sh): passthrough arguments

--- clippy.sh
+++ clippy.sh
@@ -2,4 +2,4 @@
 
 # the following line is an workaround, because of: https://github.com/rust-lang/rust-clippy/issues/4612
 find . -type f | grep "\.rs$" | xargs touch -c --
-CLIPPY_DISABLE_DOCS_LINKS=1 cargo clippy --all-features -- -D clippy::correctness -W clippy::style -W clippy::complexity -W clippy::perf -A clippy::needless_return -D clippy::implicit_return
+CLIPPY_DISABLE_DOCS_LINKS=1 cargo clippy --all-features -Z unstable-options "$@" -- -D clippy::correctness -W clippy::style -W clippy::complexity -W clippy::perf -A clippy::needless_return -D clippy::implicit_return
