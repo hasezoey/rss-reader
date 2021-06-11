fc265a3154be332b4ad13f3a649aeabcc8e5667d
hasezoey <hasezoey@gmail.com>
2021-06-11 14:35:58 +0200

    fix(main): start logger with "off" and set new level after reconfigure

--- crates/daemon/src/main.rs
+++ crates/daemon/src/main.rs
@@ -22,12 +22,13 @@ use tokio::time;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-	// TODO: change this "log_to_file" somehow to not actually write until reconfigured
-	let logger_handle = Logger::try_with_env_or_str("debug")?
+	// set "try_with_str" to "off" when not debugging anything before the logger is reconfigured (reset_flw)
+	let mut logger_handle = Logger::try_with_str("off")?
 		.log_to_file(FileSpec::default().directory(std::env::temp_dir()))
+		.print_message()
 		.duplicate_to_stderr(flexi_logger::Duplicate::All)
 		.start()
-		.context("Logger failed to start")?; // TODO: change this to "error" when ready
+		.context("Logger failed to start")?;
 	let yml = load_yaml!("../cli.yml");
 	let cli_matches = App::from_yaml(yml).get_matches();
 	let config = config::Config::from_cli_matches(&cli_matches).context("Creating config from cli/env failed")?;
@@ -44,9 +45,14 @@ async fn main() -> anyhow::Result<()> {
 			.cleanup_in_background_thread(true)
 			.append()
 	};
+	info!("Reconfiguring Logger to Config");
 	logger_handle
 		.reset_flw(&flwb)
 		.context("Logger failed to reconfigure to Config")?;
+	// TODO: change this to "error" when ready
+	logger_handle
+		.set_new_spec(flexi_logger::LogSpecification::env_or_parse("debug").context("Failed to set new logger level")?);
+	info!("Log Reconfigured");
 
 	tokio::task::spawn(async move {
 		debug!("inside sleep-interval task");
